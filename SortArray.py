73c21f9b897c1dff4385b39af13edabd7a045d6a
Psami-wondah <60843008+Psami-wondah@users.noreply.github.com>
2021-01-11 21:40:40 +0100

    Add files via upload

---
+++ SortArray.py
@@ -0,0 +1,18 @@
+Scatteredarray=[1]
+x=3
+n=2
+while len(Scatteredarray)<50001:
+    Scatteredarray.append(x)
+    x=x+2
+while len(Scatteredarray)<100000:
+    Scatteredarray.append(n)
+    n=n+2
+print(len(Scatteredarray))
+print(Scatteredarray[50001])
+#the above code created an array of 100000 scattered numbers odd one side even one side
+
+
+Sortedarray=(sorted(Scatteredarray))
+#the above code sorts the scattered numbers
+print(len(Sortedarray))
+print(Sortedarray[50001])
